8abe289f9b7766982ddc51ca6f4ebcf9c6d14ef2
admin <admin@DESKTOP-EEV2VM6>
2023-11-13 17:54:06 +0900

    aa

---
+++ middleProject/src/main/java/co/yedam/member/service/MemberVO.java
@@ -0,0 +1,14 @@
+package co.yedam.member.service;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private String mid;
+	private String name;
+	private String pass;
+	private String ssn;
+	private String phone;
+	private String email;
+	private String responsibility;
+}
